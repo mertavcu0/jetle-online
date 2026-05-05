d27af0f84aa2d3ca3c608cbfe7e624ebbe929b5d
Mert <seninmailin@gmail.com>
2026-05-05 07:44:12 +0300

    path fix real

--- jetle-online/backend/server.js
+++ jetle-online/backend/server.js
@@ -49,7 +49,7 @@ app.get("/api/test-auth", (req, res) => {
   res.json({ ok: true });
 });
 
-app.use(express.static(path.join(__dirname, "..")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/uploads", express.static("uploads"));
 
 // Test route
@@ -69,7 +69,7 @@ app.get("/", (req, res) => {
 
 // index.html fallback
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.use((err, req, res, next) => {
